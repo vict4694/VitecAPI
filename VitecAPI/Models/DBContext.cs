3a1dd13dd2b7c1dbde9faab91b0a85257e9c8f71
Victor Sebastian Mølschau Lauridsen <vict4694@edu.eal.dk>
2019-11-05 11:11:53 +0100

    Add project files.

---
+++ VitecAPI/Models/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VitecAPI.Models
+{
+    public class DBContext : DbContext
+    {
+
+        public DBContext(DbContextOptions<DBContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ VitecAPI/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecAPI.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public int Id{ get; set; }
+        public string ProductDescription{ get; set; }
+        public double Price { get; set; }
+    }
+}
